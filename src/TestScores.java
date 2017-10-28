d7e0f5c152ad5bde34ae743350d0998c060d7e8f
plooba <league@WTS13.attlocal.net>
2017-10-28 14:26:09 -0700

    high low

--- src/TestScores.java
+++ src/TestScores.java
@@ -1,7 +1,37 @@
+import javax.swing.JOptionPane;
 
 public class TestScores {
 	public static void main(String[] args) {
-		JOptionPane.showInputDialog("what did you get on you math test?");
-	}
-
+		String answer = JOptionPane.showInputDialog("what did you get on you math test?");
+		int score = Integer.parseInt(answer);
+if(score>=90){
+			JOptionPane.showMessageDialog(null,"you got an A!!");}
+else if(score>=80) {
+				JOptionPane.showMessageDialog(null, "you got a B");
 }
+else	 if(score>=70) {
+					 JOptionPane.showMessageDialog(null,"you got a C");}
+				 else if(score>=60) {
+						 JOptionPane.showMessageDialog(null, "you got a D");}
+					 else if(score<=59) {
+						 JOptionPane.showMessageDialog(null, "you failed");}
+					 }
+					 
+					 }
+					 
+		
+				
+	
+			
+		
+				
+				
+			
+			
+
+
+		
+
+
+
+
