41f228b16211afa8575c45454d0f6329d4e2eea1
100297 <league@WTS7.attlocal.net>
2017-11-04 14:29:43 -0700

    for loops

---
+++ src/forLoops.java
@@ -0,0 +1,28 @@
+
+public class forLoops {
+	public static void main(String[] args) {
+		for (int i = 1 ; i <501;i+=2) {
+			System.out.println(i+"odd");
+		System.out.println((i+1)+"even");
+				
+			}
+			
+				
+			}
+			
+				
+			
+			
+			
+			
+			
+		}
+		
+	
+		
+		
+		
+	
+	
+
+
