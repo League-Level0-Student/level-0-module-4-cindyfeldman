9179a27266a9dc531cd76499c7458916e1585b05
100297 <league@WTS7.attlocal.net>
2017-11-18 14:25:20 -0800

    loops

--- src/forLoops.java
+++ src/forLoops.java
@@ -1,13 +1,44 @@
+import javax.swing.plaf.synth.SynthSpinnerUI;
 
 public class forLoops {
 	public static void main(String[] args) {
-		for (int i = 1; i < 10; i++) {
-			for (int j = 1; j < 10; j++) {
-		System.out.println(i);
-				
+
+		
+		/*String line= "";
+		for (int i = 1; i < 101; i+=10) {
+			for (int j = 0 ; j < 10; j+=1) {
+				// System.out.println(line+" "+j);
+			line= line + " "+(i+j);
 			}
-			
+			System.out.println(line);
+			line="";*/
+		
 		}
+			//System.out.println(i+" "+(i+1)+" "+(i+2)+" "+(i+3)+" "+(i+4)+" "+(i+5)+" "+(i+6)+" "+(i+7)+" "+(i+8)+" "+(i+9));//
+		}
+			
+		
+		
+		
+		/*for (int i = 1; i < 9; i+=4) {
+			System.out.println(i+" "+(i+1)+" "+(i+2)+" "+ (i+3));
+		}
+			
+	}}*/
+		
+		
+		/*
+		  for (int i = 1; i < 10; i+=3) {
+		 
+			System.out.println(i+" "+(i+1)+" "+(i+2));
+	}}*/
+					
+
+		
+		
+			
+			
+		
 		/*
 			for (int i = 0; i < 3; i++) {
 				for (int j = 0; j < 3; j++) {
@@ -15,8 +46,8 @@ public class forLoops {
 				}
 			}
 			*/
-	}
-}
+	
+
 			
 			
 			
